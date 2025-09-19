e5dd2a1faef94dbc0f0ab45a45461d3c4280e8c3
David Sargeant <chorpler@gmail.com>
2025-09-19 00:38:00 -0600

    Add bundled dependencies for minify and prettify

---
+++ resources/included_dependencies/requests_toolbelt/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+requests-toolbelt
+=================
+
+See http://toolbelt.rtfd.org/ for documentation
+
+:copyright: (c) 2014 by Ian Cordasco and Cory Benfield
+:license: Apache v2.0, see LICENSE for more details
+
+Modified 2025 for FanFicFare including in Calibre plugin for old
+python2 versions of Calibre.
+
+FFF only needs utils.dump for cloudscraper debug output.
+"""
+
+__title__ = 'requests-toolbelt'
+__authors__ = 'Ian Cordasco, Cory Benfield'
+__license__ = 'Apache v2.0'
+__copyright__ = 'Copyright 2014 Ian Cordasco, Cory Benfield'
+__version__ = '0.9.1'
+__version_info__ = tuple(int(i) for i in __version__.split('.'))
